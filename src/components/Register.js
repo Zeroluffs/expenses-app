9f4cdc1c3de2890a5ce48e22271b75f312f88292
Pineda D´Lamarck <jdpineda@indracompany.com>
2021-11-29 17:43:53 -0500

    remove unused imports in register form

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,5 +1,4 @@
 import React, { useContext, useState } from "react";
-import { Button, Form } from "semantic-ui-react";
 import axios from "axios";
 
 import { AuthContext } from "../context/auth";
@@ -133,58 +132,5 @@ export function Register(props) {
         </div>
       </div>
     </div>
-    // <div className="form-container">
-    //   <Form onSubmit={onSubmit}>
-    //     <h1>Register</h1>
-    //     <Form.Input
-    //       label="Username"
-    //       placeholder="Username.."
-    //       name="username"
-    //       type="text"
-    //       //   value={values.username}
-    //       error={errors.username ? true : false}
-    //       onChange={onChange}
-    //     />
-    //     <Form.Input
-    //       label="Bugdet"
-    //       placeholder="Budget.."
-    //       name="budget"
-    //       type="budget"
-    //       //   value={values.email}
-    //       error={errors.budget ? true : false}
-    //       onChange={onChange}
-    //     />
-    //     <Form.Input
-    //       label="Password"
-    //       placeholder="Password.."
-    //       name="password"
-    //       type="password"
-    //       //   value={values.password}
-    //       error={errors.password ? true : false}
-    //       onChange={onChange}
-    //     />
-    //     <Form.Input
-    //       label="Confirm Password"
-    //       placeholder="Confirm Password.."
-    //       name="confirmPassword"
-    //       type="password"
-    //       //   value={values.confirmPassword}
-    //       error={errors.confirmPassword ? true : false}
-    //       onChange={onChange}
-    //     />
-    //     <Button type="submit" primary>
-    //       Register
-    //     </Button>
-    //   </Form>
-    //   {Object.keys(errors).length > 0 && (
-    //     <div className="ui error message">
-    //       <ul className="list">
-    //         {Object.values(errors).map((value) => (
-    //           <li key={value}>{value}</li>
-    //         ))}
-    //       </ul>
-    //     </div>
-    //   )}
-    // </div>
   );
 }
