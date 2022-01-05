5452c071dc02c4afd669916c6b02813b5ba1f35e
Pineda D´Lamarck <jdpineda@indracompany.com>
2022-01-05 10:24:45 -0500

    some changes

--- src/components/EditBudgetModal.js
+++ src/components/EditBudgetModal.js
@@ -5,9 +5,33 @@ export function EditBudgetModal(props) {
     }
     return (
       <div
-        className="bg-gray-500 flex justify-items-center items-center fixed  w-96 h-96"
-        onClick={props.onClose}
-      ></div>
+        className="bg-gray-100 bg-opacity-400 absolute mx-60 flex   
+      w-96 h-44 justify-center align-middle" onClick={props.onClose}
+      >
+        <div
+          className=" flex justify-items-center items-center "
+          
+        >
+          <div className="m-auto ">
+            <div
+              onClick={(e) => e.stopPropagation()}
+              className=" text-2xl mb-3"
+            >
+              New Amount
+            </div>
+            <input
+              type="number"
+              className="rounded-lg h-11 text-lg p-3"
+            ></input>
+            <button
+              onClick={props.onClose}
+              className="bg-green-500 text-lg ml-4"
+            >
+              Submit
+            </button>
+          </div>
+        </div>
+      </div>
     );
   };
 
